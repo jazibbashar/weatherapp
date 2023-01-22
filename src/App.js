538661e9035dc37d650e8dab509d276bb139bafe
Jazibbashar <jazibbashar@gmail.com>
2023-01-22 21:41:37 +0530

    Initialize project using Create React App weatherApp react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from "axios"
 
 function App() {
+  const[temp , setTemp]=useState(100);
+  const[humidity , setHumidity]=useState(0);
+  const [cityText , setCityText]=useState("")
+  async function fetchWeather(){
+    console.log("fetch Weather of"+" "+ cityText);
+  
+  //https://openweathermap.org/data/2.5/weather?q=${cityText}&appid=84f4844eb02e8696dc818c95b3e5611f
+    try{
+      let response= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityText}&appid=84f4844eb02e8696dc818c95b3e5611f`)
+      setHumidity(response.data.main.humidity)
+     setTemp(response.data.main.temp)
+    }catch(error){
+      console.log("error");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WEATHER APP</h1>
+    <div>
+    <input className="form-control" 
+    value={cityText}
+    onChange={(e)=>{
+      setCityText(e.target.value)
+    }}
+    type="text"/>
+    <button onClick={fetchWeather} style={{marginTop:"10px"}}className="btn btn-outline-info">Submit</button>
+    </div>
+    <div className='box'>
+    <h3> temp :  {temp} F</h3>
+    <h3> humidity :  {humidity} </h3>
+    </div>
     </div>
   );
 }
-
 export default App;
